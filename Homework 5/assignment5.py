d2ba56fe8227901a6c58196765fcfc89d6c30e8f
aryarahmanian <aryarahmanian@tamu.edu>
2023-04-11 16:22:53 -0500

    done with homework 5

--- Homework 5/assignment5.py
+++ Homework 5/assignment5.py
@@ -21,12 +21,12 @@ import os
 
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
-from sklearn.model_selection import StratifiedKFold, cross_val_score
+from sklearn.model_selection import StratifiedKFold, cross_val_score , KFold
 from sklearn.metrics import roc_auc_score, f1_score
 from sklearn.metrics import accuracy_score
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression, Ridge, Lasso
+from sklearn.linear_model import LassoCV, LogisticRegression, Ridge, Lasso
 from sklearn.model_selection import GridSearchCV
 from typing import Tuple, List
 from typeguard import typechecked
@@ -149,20 +149,17 @@ def qd3_visualize(dataset:np.ndarray, pca:PCA, dim_x = 243, dim_y = 320):
     plt.figure(figsize=(14, 7))
     for i, k in enumerate(num_components):
         #train pcs for num of components
-        
-        pca1 = PCA(n_components=k)
-        pca1.fit(dataset)
+        pca = PCA(n_components=k)
+        pca.fit(dataset)
         #work with img1
-        img1_reconstructed = qd2_reconstruct(qd1_project(dataset, pca1)[img_idx1], pca1).reshape(dim_x, dim_y)
+        img1_reconstructed = qd2_reconstruct(qd1_project(dataset, pca)[img_idx1], pca).reshape(dim_x, dim_y)
         plt.subplot(2, 7, i+1)
         plt.imshow(img1_reconstructed, cmap='gray')
         plt.axis('off')
         plt.title(f'k={k}')
 
-
-        print(qd2_reconstruct(qd1_project(dataset, pca)[img_idx2], pca))
         #plot image 2
-        img2_reconstructed =  qd2_reconstruct(qd1_project(dataset, pca)[img_idx2][:k], pca)
+        img2_reconstructed = qd2_reconstruct(qd1_project(dataset, pca)[img_idx2], pca).reshape(dim_x, dim_y)
         plt.subplot(2, 7, i+8)
         plt.imshow(img2_reconstructed, cmap='gray')
         plt.axis('off')
@@ -208,37 +205,30 @@ def qe2_lasso(trainX:np.ndarray, trainY:np.ndarray, pca:PCA) -> Tuple[int, float
 
     Hint: you can pick 5 `k' values uniformly distributed
     """
-    #split
-    trainX, testX, trainY, testY = train_test_split(trainX[:, :-1], trainX[:, -1], test_size=0.2)
+    #init k values
+    num_components = list(range(10, 101, 20))
+    best_k = None
+    best_acc = 0.0
+    
+    #split data
+    X_train, X_test, y_train, y_test = train_test_split(trainX, trainY, test_size=0.2)
+    
+    for k in num_components:
+        #change num of components
+        pca.n_components = k
+        pca.fit(X_train)
+        train_transformed = pca.transform(X_train)
+        test_transformed = pca.transform(X_test)
 
-    pca = PCA().fit(trainX)
+        #train and do 5 fold CV
+        lasso = LassoCV(cv=5).fit(train_transformed, y_train)
+        acc = lasso.score(test_transformed, y_test)
 
-    #init k vals
-    k_values = np.arange(10, 101, 20)
-
-    best_k = 0
-    best_accuracy = 0.0
-
-    for k in k_values:
-        #project the training set onto the first k components
-        trainX_pca = pca.transform(trainX)[:, :k]
-
-        #train lasso with projected data
-        lasso = Lasso(alpha=0.1)
-        lasso.fit(trainX_pca, trainY)
-
-        testX_pca = pca.transform(testX)[:, :k]
-
-        accuracy = lasso.score(testX_pca, testY)
-        
-        print(accuracy)
-        #store best values
-        if accuracy > best_accuracy:
+        if acc > best_acc:
+            best_acc = acc
             best_k = k
-            best_accuracy = accuracy
 
-    return int(best_k), best_accuracy
-
+    return best_k, best_acc
 
 
 if __name__ == "__main__":
@@ -247,13 +237,13 @@ if __name__ == "__main__":
     dataset = qa2_preprocess(faces)
     pca, eig_values, eig_vectors = qa3_calc_eig_val_vec(dataset, len(dataset))
 
-    #qb_plot_written(eig_values)
+    qb_plot_written(eig_values)
 
     num = len(dataset)
     org_dim_eig_faces = qc1_reshape_images(pca)
-    #qc2_plot(org_dim_eig_faces)
+    qc2_plot(org_dim_eig_faces)
 
-    #qd3_visualize(dataset, pca)
+    qd3_visualize(dataset, pca)
     best_k, result = qe1_svm(dataset, y_target, pca)
     print(best_k, result)
     best_k, result = qe2_lasso(dataset, y_target, pca)
