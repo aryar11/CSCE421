7bc60e45bcde28ecb7e09d2a9fed146df56aa1ce
aryarahmanian <aryarahmanian@tamu.edu>
2023-05-05 23:19:02 -0500

    done with final project

--- Capstone project/data_import.py
+++ Capstone project/data_import.py
@@ -7,28 +7,6 @@ from scrapy import Selector
 
 
 
-class Scraper:
-
-    def __init__(self):
-        self.sess = requests.session()
-
-    def start(self):
-        if not os.path.exists("Output"):
-            os.mkdir("Output")
-        self.login()
-        for item in links_to_download:
-            self.download_file(item)
-        print("Completed!")
-
-    def login(self):
-        print("logging into website........")
-        r = self.sess.get(LOGIN_URL, headers={"User-Agent": headers.get("User-Agent")})
-        response = Selector(text=r.content)
-        csrf_token = response.xpath("//input[@name='spacetrack_csrf_token']/@value").get()
-        self.sess.post(LOGIN_URL, headers=headers, data=payload.format(csrf_token))
-        print("logged in!")
-
-
 today = date.today()
 formatted_date = today.strftime("%Y-%m-%d") #todays date
 year = formatted_date[:4]
@@ -68,11 +46,16 @@ csrf_token = response.xpath("//input[@name='spacetrack_csrf_token']/@value").get
 sess.post(LOGIN_URL, headers=headers, data=payload.format(csrf_token))
 
 #######IMPORT 3LE########
+print("Downloading 3le")
 url = "https://www.space-track.org/basicspacedata/query/class/gp/EPOCH/%3Enow-30/orderby/NORAD_CAT_ID,EPOCH/format/3le"
 
 # Set the file path and name
 file_path = r"C:\SWAT\Data\TLE\Import\3le.txt"
 r = sess.get(url, headers={"User-Agent": headers.get("User-Agent")})
+if r.status_code == requests.codes.ok:
+    print("Request successful")
+else:
+    print(f"Request failed with status code {r.status_code}")
 # Write the TLEs to the file
 with open(file_path, "wb") as file:
     file.write(r.content)
@@ -82,13 +65,14 @@ with open(file_path, "r") as file:
     lines = file.readlines()
 
 with open(file_path, "w") as file:
-    for i, line in enumerate(lines):
-        if line.startswith("0 TBA"):
-            file.writelines(lines[:i])
-            break
-    else:
-        # If no line starts with "0 TBA", write all lines back to the file
-        file.writelines(lines)
+    i = 0
+    while i < len(lines):
+        if lines[i].startswith("0 TBA"):
+            del lines[i:i+3]  # Remove the current line and the two following lines
+        else:
+            i += 1
+
+    file.writelines(lines)
 
 # Copy the file to the date directory
 shutil.copy(file_path, date_directory)
@@ -102,9 +86,12 @@ yesterday = today - one_day
 formatted_tomorrow = tomorrow.strftime("%Y-%m-%d") #tmrws date
 formatted_yesterday = yesterday.strftime("%Y-%m-%d") #yesterday date
 url ="https://www.space-track.org/basicspacedata/query/class/gp_history/CREATION_DATE/" + formatted_yesterday + "--" + formatted_date + "/orderby/NORAD_CAT_ID,EPOCH/format/tle/emptyresult/show"
-
+print("Downloading TLE")
 r = sess.get(url, headers={"User-Agent": headers.get("User-Agent")})
-
+if r.status_code == requests.codes.ok:
+    print("Request successful")
+else:
+    print(f"Request failed with status code {r.status_code}")
 # Set the file path and name
 file_path = r"C:\SWAT\Data\TLE\Import\tle.txt"
 
@@ -115,12 +102,16 @@ with open(file_path, "wb") as file:
 # Copy the file to the date directory
 shutil.copy(file_path, date_directory)
 
-    
-
 
 ####IMPORT SATELLITE DATA.CSV AND XLSX
+print("Downloading Satellite Data")
 url = "https://www.space-track.org/basicspacedata/query/class/satcat/predicates/OBJECT_ID,OBJECT_NAME,NORAD_CAT_ID,COUNTRY,PERIOD,INCLINATION,APOGEE,PERIGEE,RCS_SIZE,RCSVALUE,LAUNCH,COMMENT/DECAY/null-val/CURRENT/Y/orderby/NORAD_CAT_ID%20desc/format/csv/emptyresult/show"
 r = sess.get(url, headers={"User-Agent": headers.get("User-Agent")})
+
+if r.status_code == requests.codes.ok:
+    print("Request successful")
+else:
+    print(f"Request failed with status code {r.status_code}")
 # Set the file path and name
 file_path = r"C:\SWAT\Data\TLE\Import\Satellite Data.csv"
 
@@ -143,8 +134,10 @@ shutil.copy(file_path, date_directory)
 
 
 ##RUN UPDATE Command##
+print("Updating Microsoft Databases")
 accessApp = win32com.client.Dispatch("Access.Application")
 accessApp.OpenCurrentDatabase("C:\\SWAT\\Data\\SID\\SWAT-Data-TLE.mdb")
 accessApp.DoCmd.RunMacro("SSC-Import-TLE")
 accessApp.CloseCurrentDatabase()
 accessApp.Quit()
+print("Done with updating satellite data")
